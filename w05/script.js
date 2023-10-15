488398e57d4054be106e3909c1b3a515c62bc0b0
rlarkdms-d <144655694+rlarkdms-d@users.noreply.github.com>
2023-10-15 19:35:01 +0900

    Add files via upload

---
+++ w05/script.js
@@ -0,0 +1,12 @@
+function add(left, right){
+    return left + right;
+  }
+  function sub(left, right){
+    /* 내용 작성 */
+  }
+  function mul(left, right){
+    /* 내용 작성 */
+  }
+  function div(left, right){
+    /* 내용 작성 */
+  }
